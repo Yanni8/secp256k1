c5f5e73bb638ef02c6c6d9cfbc1243fc52bce6d8
Yanni8 <99135388+Yanni8@users.noreply.github.com>
2022-06-21 21:41:33 +0200

    Update secp256k1.py

--- secp256k1.py
+++ secp256k1.py
@@ -1,5 +1,8 @@
 """
 © Yanni8 https://github.com/Yanni8
+
+This algorithmus is not efficient and also probaly not 100% secure. 
+Attackers could use Timing Attack (https://en.wikipedia.org/wiki/Timing_attack) to get informations about the private key 
 """
 
 a = 0
@@ -12,15 +15,14 @@ G = (Gx, Gy)
 
 def point_add(p1 : tuple, p2 : tuple) -> tuple:
     if p1 != p2:
-        lam = (p1[1] - p2[1]) * pow(p1[0] - p2[0], -1, p)
+        lam = (p1[1] - p2[1]) * pow(p1[0] - p2[0], p-2, p)
         x3 = (pow(lam, 2) - p1[0] - p2[0]) % p
         y3 = (lam * (p1[0] - x3) - p1[1]) % p
         return (x3, y3)
     return point_dubl(p1)
 
 def point_dubl(p1 : tuple) -> tuple:
-    print(p1[1])
-    lam = (3*p1[0]**2 + a) * pow(2*p1[1], -1, p)
+    lam = (3*p1[0]**2 + a) * pow(2*p1[1], p-2, p)
     v = p1[1] - lam*p1[0] % p
     x3 = (lam**2 - 2*p1[0]) % p
     y3 = (lam*x3 + v) * -1 % p
@@ -28,20 +30,14 @@ def point_dubl(p1 : tuple) -> tuple:
 
 
 def calc_publ(private_key):
-    publ = (Gx, Gy)
+    publ = None
     Q = (Gx, Gy)
-    binar = bin(private_key)
+    binar = private_key
     while binar:
         Q = point_dubl(Q)
+        if publ == None:
+            publ = Q
         if binar%2 == 1:
             publ = point_add(publ, Q)
         binar = binar//2    
     return publ  
-
-
-def example_test():
-    private_key = "0x69"
-    publ = calc_publ(private_key)
-    print(publ)
-    assert publ == (0xf219ea5d6b54701c1c14de5b557eb42a8d13f3abbcd08affcc2a5e6b049b8d63,
-                    0x4cb95957e83d40b0f73af4544cccf6b1f4b08d3c07b27fb8d8c2962a400766d1)
