971399dc574fe57eaa7cf9010be411b9ec70bb08
Yanni8 <99135388+Yanni8@users.noreply.github.com>
2022-06-22 16:45:09 +0200

    Changed Parameter

--- secp256k1.py
+++ secp256k1.py
@@ -1,29 +1,29 @@
 """
 © Yanni8 https://github.com/Yanni8
-This algorithmus is not efficient and also probaly not 100% secure. 
+This algorythmus is not efficient and also probaly not 100% secure. 
 Attackers could use Timing Attack (https://en.wikipedia.org/wiki/Timing_attack) to get informations about the private key 
 """
 
-a = 0
-b = 7
-p = 2**256 - 2**32 - 2**9 - 2**8 - 2**7 - 2**6 - 2**4 - 1
-Gx = 55066263022277343669578718895168534326250603453777594175500187360389116729240
-Gy = 32670510020758816978083085130507043184471273380659243275938904335757337482424
-G = (Gx, Gy)
+A = 0
+B = 7
+P = 2**256 - 2**32 - 2**9 - 2**8 - 2**7 - 2**6 - 2**4 - 1
+GX = 55066263022277343669578718895168534326250603453777594175500187360389116729240
+GY = 32670510020758816978083085130507043184471273380659243275938904335757337482424
+G = (GX, GY)
 
 def point_add(p1 : tuple, p2 : tuple) -> tuple:
     if p1 != p2:
-        lam = (p1[1] - p2[1]) * pow(p1[0] - p2[0], p-2, p)
-        x3 = (pow(lam, 2) - p1[0] - p2[0]) % p
-        y3 = (lam * (p1[0] - x3) - p1[1])  % p
+        lam = (p1[1] - p2[1]) * pow(p1[0] - p2[0], P-2, P)
+        x3 = (pow(lam, 2) - p1[0] - p2[0]) % P
+        y3 = (lam * (p1[0] - x3) - p1[1])  % P
         return (x3, y3)
     return point_dubl(p1)
 
 def point_dubl(p1 : tuple) -> tuple:
-    lam = (3*p1[0]**2 + a) * pow(2*p1[1], p-2, p)
-    v = p1[1] - lam*p1[0] % p
-    x3 = (lam**2 - 2*p1[0]) % p
-    y3 = (lam*x3 + v) * -1 % p
+    lam = (3*p1[0]**2 + A) * pow(2*p1[1], P-2, P)
+    v = p1[1] - lam*p1[0] % P
+    x3 = (lam**2 - 2*p1[0]) % P
+    y3 = (lam*x3 + v) * -1 % P
     return (x3, y3)
 
 
@@ -40,4 +40,3 @@ def calc_publ(private_key):
         Q = point_dubl(Q)
         binar = binar//2    
     return publ  
-
